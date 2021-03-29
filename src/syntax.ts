dbceaf97667071c19201b2a394c90809cef694c3
William Lahti <wilahti@gmail.com>
2021-03-29 00:37:12 -0700

    add number annot

--- src/syntax.ts
+++ src/syntax.ts
@@ -30,7 +30,7 @@ export class Packet extends ST291.Packet {
     @Marker() $payloadDescriptorStart;
 
     @Reserved(3) reserved;
-    @Field(2, { writtenValue: 1 }) version = 1;
+    @Field(2, { writtenValue: 1 }) version : number = 1;
     @Field(1) continued : boolean;
     @Field(1) following : boolean;
     @Field(1) duplicate : boolean;
